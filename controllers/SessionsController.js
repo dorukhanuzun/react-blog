31b55964072e5c738d694d5f9c4e87fed33d82ea
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2020-07-13 14:40:57 -0400

    Authentication in React

--- controllers/SessionsController.js
+++ controllers/SessionsController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const passport = require('passport');
 const viewPath = 'sessions';
+const jwt = require('jsonwebtoken');
 
 exports.new = (req, res) => {
   res.render(`${viewPath}/new`, {
@@ -9,11 +10,31 @@ exports.new = (req, res) => {
 };
 
 exports.create = (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/blogs',
-    successFlash: 'You were successfully logged in.',
-    failureRedirect: '/login',
-    failureFlash: 'Invalid credentials'
+  console.log(req.body);
+  passport.authenticate('local', (err, user) => {
+    if (err || !user) return res.status(401).json({
+      status: 'failed',
+      message: 'Not authorized',
+      error: err
+    });
+
+    req.login(user, err => {
+      if (err) return res.status(401).json({
+        status: 'failed',
+        message: 'Not authorized',
+        error: err
+      });
+
+      delete user.password;
+      const token = jwt.sign({user: user}, 'superSecretSaltKey');
+      res.cookie('token', token, {httpOnly: true});
+
+      return res.status(200).json({
+        status: 'success',
+        message: 'Logged in successfully',
+        user
+      })
+    })
   })(req, res, next);
 };
 
